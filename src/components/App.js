d5ac6e68f01eb31f53e5260effda52498f59d533
Master <a@d3m.ru>
2018-10-10 11:58:40 +0300

    Layout for sign in page with 'Sign In' and 'Register' tabs

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 
 import ChatPage from './ChatPage';
 import WelcomePage from './WelcomePage';
+import WelcomeTabs from './WelcomeTabs';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/chat" component={ChatPage}/>
-      <Route exact path="/(welcome)?" component={WelcomePage}/>
+      <Route exact path="/(welcome)?" component={WelcomeTabs}/>
       <Redirect to="/" />
     </Switch>
   </Router>
